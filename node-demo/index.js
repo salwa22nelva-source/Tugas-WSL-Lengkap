7335b4bc0e0e11b083f12d80736ff1d0532c2d3b
salwa22nelva-source <salwa22nelva@gmail.com>
2026-04-06 13:43:33 +0700

    Tugas Instalasi Runtime Nodejs dan Python

--- node-demo/index.js
+++ node-demo/index.js
@@ -1,3 +1,14 @@
-import requests
-response = requests.get('https://api.github.com')
-print(f"Status: {response.status_code}")
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Halo dari WSL Node.js di Browser!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
